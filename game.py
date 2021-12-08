152b99a42eeb3e5b2e397916f4b8b5562b6d84be
Rae Carter <92331343+heimdalalr@users.noreply.github.com>
2021-12-07 21:02:56 -0500

    adds check to see if guess is an int

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
-"""A number-guessing game."""
+# """A number-guessing game."""
 
-# Put your code here
+import random
+def check_if_int(initguess):
+    while type(initguess) != "int":
+        try:
+            x = int(initguess)
+        except:
+            print(initguess + ' isn\'t a number')
+            initguess = input('Your guess?')
+        else:
+            initguess = int(initguess)
+            break
+    guess = initguess
+    return guess
+
+
+name = input("Hi, whats your name?")
+print('hello,  {name}')
